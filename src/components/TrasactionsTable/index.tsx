66b6f0a6a94d3580e1edb68b3bd524176707af06
Lais <lais.batista.pereira@gmail.com>
2021-11-11 17:14:51 -0300

    :alien: List transactions and seeds

--- src/components/TrasactionsTable/index.tsx
+++ src/components/TrasactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   useEffect(() => {
-    api.get('transactions').then(response => console.log(response.data))
+    api.get('transactions')
+    .then(response => setTransactions(response.data.transactions))
   }, []);
 
   return(
@@ -20,18 +31,14 @@ export function TransactionTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Coopersystem</td>
-            <td className="deposit">R$12.000,00</td>
-            <td>Trabalho</td>
-            <td>07/11/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$2.000,00</td>
-            <td>Trabalho</td>
-            <td>07/11/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
 
       </table>
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,37 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({ 
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer com Thiago',
+          type: 'deposit',
+          category: 'Job',
+          amount: 6000,
+          createdAt: new Date('2021-11-11 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Carro',
+          type: 'withdraw',
+          category: 'Luxo',
+          amount: 2000,
+          createdAt: new Date('2021-11-11 09:00:00'),
+        },
+        {
+          id: 3,
+          title: 'Freelancer com Júlio',
+          type: 'deposit',
+          category: 'Job',
+          amount: 10000,
+          createdAt: new Date('2021-10-11 09:00:00'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
