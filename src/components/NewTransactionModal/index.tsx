bb3eea06b4a1b24c22ec5298f818043c15f91586
Lais <lais.batista.pereira@gmail.com>
2021-11-11 20:00:16 -0300

    :adhesive_bandage:Fix new Data()

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,9 @@ import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { api } from "../../services/api";
+import { TransactionsContext } from "../../context/TransactionContext";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,23 +13,25 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const transactions = useContext(TransactionsContext);
+
+
   const [title, setTitle] = useState('');
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('');
 
-
-  const data ={
-    title,
-    value, 
-    category, 
-    type
-  };
-
-  api.post('/transactions', data);
-
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
+
+    const data ={
+      title,
+      value, 
+      category, 
+      type
+    };
+
+    api.post('/transactions', data);
   }
 
   return (
